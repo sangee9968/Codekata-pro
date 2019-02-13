ad28ceb6c33548e2aa8ea1b913b440ef4d2f267d
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-13 05:59:58 -0800

    Update pr3.py

--- pr3.py
+++ pr3.py
@@ -1,9 +1,15 @@
 s1,s2=map(str,input().split())
 l1=list(s1)
 l2=list(s2)
-l3=[]
 for x, y in zip(s1,s2):
-   if x==y:
-     l2.remove(y)  
-#result     
+  if len(s1)>len(s2) and x==y:
+    l1.remove(y)
+    print(len(l1))
+  else:
+    break
+for x, y in zip(s1,s2):  
+  if len(s1)<=len(s2) and x==y:
+    l2.remove(y)
+#result      
 print(len(l2))
+  
