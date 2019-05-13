06fa0791ecf86689438fb9ea2fc47af92353383f
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-13 14:31:18 +0530

    Update pr7.py

--- pr7.py
+++ pr7.py
@@ -1,6 +1,13 @@
 n=int(input())
-if n%2!=0:
-	print("1")
-else:
-  #result
+l=[1,2,4,8,16,32,64,128,256,512,1024,2048,4096]
+if n&(n-1)==0:
 	print("0")
+l1=[]
+if n&(n-1)!=0 :
+	for i in l:
+		if n>i:
+			l1.append(i)
+#result			
+print(n-max(l1))    
+
+	
