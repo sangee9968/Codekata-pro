9ccf1109d97828a38b82aef199f30ffc0a7b94a6
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-14 16:28:40 +0530

    Update pr61.py

--- pr61.py
+++ pr61.py
@@ -1,6 +1,12 @@
-n=input()
-m=input()
-s=[]
-for x,y in zip(n,m):
-    s.append(chr(ord('a')+((ord(x)-ord('a')+ord(y)-ord('a'))%26)+1))
-print("".join(s)) 
+s1=input()
+s2=input()
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+l1=[]
+for i,j in zip(s1,s2):
+	l1.append((l.index(i)+1)+(l.index(j)+1))
+for i in range(0,len(l1)):
+	if l1[i]>26:
+		l1[i]=l1[i]%26
+for i in l1:
+    #result
+	print(l[i-1],end="")
