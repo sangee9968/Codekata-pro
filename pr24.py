6ee599fe4e8da04d50df844924039294712e2e12
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-17 09:27:27 +0530

    Update pr24.py

--- pr24.py
+++ pr24.py
@@ -1,6 +1,14 @@
 n=int(input())
-p=2**(n)
-l=[]
-for i in range(0,p):
-	l.append(bin(i))
-print(l)	
+a=[]
+k=bin(2**n-1)[2::]
+l=len(k)
+for i in range(0,2**n):
+	s=bin(i)[2::]
+	if len(s)<l:
+		a.append([s.count("1"),(l-len(s))*"0"+s])
+	else:
+		a.append([s.count("1"),s])
+#sort list		
+a.sort()
+for i in range(0,len(a)):
+	print(a[i][1])
